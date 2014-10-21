6668aecaf8c600922e538f90b217076a542867db
Simon <zhaohuaipeng@yahoo.com>
2014-10-21 23:09:02 +0800

    first modify

--- test_SAX/src/com/meixin/xml/PraseXML.java
+++ test_SAX/src/com/meixin/xml/PraseXML.java
@@ -2,11 +2,9 @@ package com.meixin.xml;
 
 import java.io.File; 
 import java.util.Vector; 
-
 import org.xml.sax.Attributes; 
 import org.xml.sax.SAXException; 
 import org.xml.sax.helpers.DefaultHandler; 
-
 import javax.xml.parsers.SAXParser; 
 import javax.xml.parsers.SAXParserFactory; 
 
